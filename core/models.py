44c4c68884d00d22ea0d2b26de80a6e812c583f3
MohammadReza Vafazadeh <mamadbarcay@gmail.com>
2021-06-11 12:54:34 +0430

    Created tag and post model in core app

--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    status = models.CharField(
+        choices=[('non_status', 'No status'),
+                 ('accepted', 'Accepted'),
+                 ('rejected', 'Rejected')],
+        max_length=10,
+        default='non_status',
+        blank=True,
+    )
+    body = models.TextField()
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.title
