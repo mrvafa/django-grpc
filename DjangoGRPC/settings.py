477c7423b42ed41035f28ef0847197e38c40f6ac
MohammadReza Vafazadeh <mamadbarcay@gmail.com>
2021-06-11 18:24:55 +0430

    Added .env file

--- DjangoGRPC/settings.py
+++ DjangoGRPC/settings.py
@@ -9,18 +9,18 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-29ts4ks55i2df$kv0#huo@h!-_acz0&mz)&4n*5cgy))5#rpr#'
+SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
