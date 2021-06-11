b3506195d9e7a1587e883102551b5beda960c1b7
MohammadReza Vafazadeh <mamadbarcay@gmail.com>
2021-06-11 17:04:50 +0430

    Added example for Search

--- client.py
+++ client.py
@@ -4,7 +4,12 @@ import post_pb2
 
 channel = grpc.insecure_channel('localhost:50051')
 client = post_pb2_grpc.PostServiceStub(channel)
-# request = post_pb2.SearchText(search_key='p 5')
 request = post_pb2.PostIDRequest(id=1)
 respond = client.GetPostByID(request)
 print(respond.title, respond.id)
+
+request = post_pb2.SearchText(search_key='5')
+posts = client.Search(request).posts
+for post in posts:
+    print(post.id, post.title, post.body)
+
